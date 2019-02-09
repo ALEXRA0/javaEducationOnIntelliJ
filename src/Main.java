90a5ae4e8f8db7fd576f5e6fd8d82b3a152efb96
ALEXRA0 <alex_ra0@Mail.ru>
2019-02-09 19:39:39 +0500

    commit for creating repository

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        ArrayList<String> myArray = new ArrayList<>(0);
+        myArray.add("Hello") ;
+        System.out.println(myArray.get(0));
+    }
+}
